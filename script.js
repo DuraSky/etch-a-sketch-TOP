30154eb825032442213808c77317d0b8ee5acc83
Lukas Broskovic <lukas.broskovic@gmail.com>
2023-10-30 09:31:54 +0100

    finilly managed to make my square creation dynamic, now to fix css

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-function createSquare(){
-    const canvas = document.querySelector("#canvas");
-    let square = document.createElement("div");
-    square.classList.add("canvasSquare");
-    // square.textContent = "testing";
-    canvas.appendChild(square);
-    
-    square.addEventListener("mouseover", () => {
-    square.style.backgroundColor = "black";
-});
-}
+let size = 64;
+let numberOfSquares = size * size;
+const canvas = document.querySelector("#canvas");
 
 
-for (let i = 0; i < 256; i++){
-    createSquare();
-}
 
+
+  for (let i = 0; i < size; i++){     
+   const row = document.createElement("div");
+   row.classList.add("row");
+
+    for (let y = 0; y < size; y++){ 
+      const square = document.createElement("div"); 
+      square.classList.add("canvasSquare");     
+      row.appendChild(square);      
+   } 
+   canvas.appendChild(row);   
+};
+
+
+     
