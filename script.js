63a130391affb1663ba7d5ceda784271c36cbd7e
Lukas Broskovic <lukas.broskovic@gmail.com>
2023-10-30 09:54:47 +0100

    made canvas div style dynamic based on how many squares the users wants

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
-let size = 64;
-let numberOfSquares = size * size;
+let size = 32;
+let canvasSize = (size * 20) + (size * 2);
+let sizeInPx = canvasSize + "px";
+console.log(sizeInPx);
 const canvas = document.querySelector("#canvas");
-
+document.querySelector("#canvas").style.width = sizeInPx;
+document.querySelector("#canvas").style.height = sizeInPx;
 
 
 
   for (let i = 0; i < size; i++){     
    const row = document.createElement("div");
    row.classList.add("row");
-
+    
     for (let y = 0; y < size; y++){ 
       const square = document.createElement("div"); 
       square.classList.add("canvasSquare");     
-      row.appendChild(square);      
+      row.appendChild(square); 
+
+      square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "black";
+      });      
    } 
    canvas.appendChild(row);   
 };
+
 
 
      
