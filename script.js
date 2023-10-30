697b1cb1a8f528901a3045e04336d8f44d56ed12
Lukas Broskovic <lukas.broskovic@gmail.com>
2023-10-30 22:31:58 +0100

    thought I was done with the exercise, however Ive made my code to resize the grid however it should resize the squares instead..have to start over.

--- script.js
+++ script.js
@@ -1,29 +1,66 @@
-let size = 32;
+let size = 16;
 let canvasSize = (size * 20) + (size * 2);
 let sizeInPx = canvasSize + "px";
-console.log(sizeInPx);
+console.log(size);
+
 const canvas = document.querySelector("#canvas");
-document.querySelector("#canvas").style.width = sizeInPx;
-document.querySelector("#canvas").style.height = sizeInPx;
+canvas.style.width = sizeInPx;
+canvas.style.height = sizeInPx;
 
-
-
-  for (let i = 0; i < size; i++){     
-   const row = document.createElement("div");
-   row.classList.add("row");
-    
-    for (let y = 0; y < size; y++){ 
+function createCanvas (){
+for (let i = 0; i < size; i++){ 
+      const row = document.createElement("div");
+      row.classList.add("row");
+   for (let y = 0; y < size; y++){ 
       const square = document.createElement("div"); 
-      square.classList.add("canvasSquare");     
+      square.classList.add("canvasSquare"); 
       row.appendChild(square); 
 
       square.addEventListener("mouseover", () => {
       square.style.backgroundColor = "black";
-      });      
+      }); 
    } 
-   canvas.appendChild(row);   
+   canvas.appendChild(row); 
+};
 };
 
+createCanvas();
+
+const button = document.querySelector("#resizeMe");
+button.addEventListener("click", () => {
+   canvas.querySelectorAll("div").forEach(div => {
+      div.remove();
+   });
+
+   let userInputSize = prompt("Pick a grid size!");
+   let userInputSizeFormula = (userInputSize * 20) + (userInputSize * 2);
+   let userInputSizeInPx = userInputSizeFormula + "px";
+   console.log(userInputSizeInPx);
+   canvas.style.width = userInputSizeInPx;
+   canvas.style.height = userInputSizeInPx;
+
+   if (userInputSize > 100){
+      alert("This is too much! You have to choose a number bellow 100!");
+      canvas.style.width = sizeInPx;
+      canvas.style.height = sizeInPx;
+      createCanvas();
+      return;
+   };
 
 
-     
+   for (let i = 0; i < userInputSize; i++){ 
+      const row = document.createElement("div");
+      row.classList.add("row");
+   for (let y = 0; y < userInputSize; y++){ 
+      const square = document.createElement("div"); 
+      square.classList.add("canvasSquare"); 
+      row.appendChild(square); 
+
+      square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "black";
+      }); 
+   } 
+   canvas.appendChild(row); 
+};   
+
+});
